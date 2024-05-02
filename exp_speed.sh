8c856f0c9511dcd31c05b088094169e4983b1840
MelodicDrumstep <1724662527@qq.com>
2024-05-02 15:29:56 +0800

    restructure the files

--- exp_speed.sh
+++ exp_speed.sh
@@ -1,16 +1,16 @@
 echo "testing the correctness of the new one"
-gcc -mavx -fopenmp -o test user_test.c matrix.c
+gcc -mavx -fopenmp -o test testing/user_test.c matrix.c
 ./test
 echo "double test the matrix multiplication"
-gcc -mavx -fopenmp -o t test_matmul.c matrix.c
+gcc -mavx -fopenmp -o t testing/test_matmul.c matrix.c
 ./t
 echo "test pass!"
 echo "testing old matrix"
-gcc -o sold speed_testing_old.c matrix_old.c
+gcc -o sold testing/speed_testing_old.c matrix_old.c
 ./sold
 echo "This is -O3 Optimization"
-gcc -O3 -o so3 speed_testing_old.c matrix_old.c
+gcc -O3 -o so3 testing/speed_testing_old.c matrix_old.c
 ./so3
 echo "testing new matrix"
-gcc -mavx -fopenmp -o snew speed_testing.c matrix.c
+gcc -mavx -fopenmp -o snew testing/speed_testing.c matrix.c
 ./snew
