e66c7507c416f9256e85e036a2a2b329759d6f9c
MelodicDrumstep <1724662527@qq.com>
2024-05-02 15:35:56 +0800

    renew the path included in the script

--- exp_speed.sh
+++ exp_speed.sh
@@ -1,16 +1,16 @@
 echo "testing the correctness of the new one"
-gcc -mavx -fopenmp -o test testing/user_test.c matrix.c
+gcc -mavx -fopenmp -I. -o test testing/user_test.c matrix.c
 ./test
 echo "double test the matrix multiplication"
-gcc -mavx -fopenmp -o t testing/test_matmul.c matrix.c
+gcc -mavx -fopenmp -I. -o t testing/test_matmul.c matrix.c
 ./t
 echo "test pass!"
 echo "testing old matrix"
-gcc -o sold testing/speed_testing_old.c matrix_old.c
+gcc -I. -o sold testing/speed_testing_old.c matrix_old.c
 ./sold
 echo "This is -O3 Optimization"
-gcc -O3 -o so3 testing/speed_testing_old.c matrix_old.c
+gcc -O3 -I. -o so3 testing/speed_testing_old.c matrix_old.c
 ./so3
 echo "testing new matrix"
-gcc -mavx -fopenmp -o snew testing/speed_testing.c matrix.c
+gcc -mavx -fopenmp -I. -o snew testing/speed_testing.c matrix.c
 ./snew
